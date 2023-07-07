52e53da9fc5343ce771e20ce4319be47418d589c
Marcelo Andrés <marcelo.lam@alumnos.ucn.cl>
2023-07-07 18:56:23 -0400

    Métodos avanzados ya modificados manualmente en el sistema.

--- MetodosDePrueba/Program.cs
+++ MetodosDePrueba/Program.cs
@@ -6,11 +6,38 @@ namespace MetodosDePrueba
     {
         static void Main(string[] args)
         {
+            // Por el momento dejaremos esta sección de código como comentario para probar con otros métodos.
             // Vamos a trabajar con una suma de 2 números en este Main de prueba.
 
-            int num1 = int.Parse(Console.ReadLine()); // Valor 1.
-            int num2 = int.Parse(Console.ReadLine()); // Valor 2.
-            Console.WriteLine($"La suma total de 2 números es: {num1 + num2}"); // Imprime la suma total (Sólo debes escribir 2 números consecutivos cualquiera en la ventana de comandos).
+            // int num1 = int.Parse(Console.ReadLine()); // Valor 1.
+            // int num2 = int.Parse(Console.ReadLine()); // Valor 2.
+            // Console.WriteLine($"La suma total de 2 números es: {num1 + num2}"); // Imprime la suma total (Sólo debes escribir 2 números consecutivos cualquiera en la ventana de comandos).
+
+            // Ahora procedemos con una variable para sumar 2 números en este método.
+
+            // double total = Suma(2, 3); // Esta línea de código tiene que tener un método estático.
+            // Console.WriteLine(total); // Imprime desde la variable la suma total.
+            Suma();
+        }
+
+        // public double[] Suma()
+        // {
+            // return new double[] { 5, 5 }; // Pero no es la forma correcta.
+            // Console.WriteLine("También hay otra forma para poder sumar con este método");
+        // }
+
+        public static void Suma()
+        {
+            // Console.WriteLine($"La suma de dos números es {num1 + num2}"); // Imprime la suma total de dos números.
+
+            // return num1 + num2;
+
+            // Existen diferentes formas de sumar dos números fundamentales utilizando variables ya avanzadas.
+
+            int num1 = int.Parse(Console.ReadLine()); // Número 1
+            int num2 = int.Parse(Console.ReadLine()); // Número 2
+            Console.WriteLine($"La suma total de dos números es: {num1 + num2}"); // Suma total.
+
         }
     }
 }
